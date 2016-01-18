06c177a2558c6a9d0f45692b6c31f1e8d9761817
praekiko <rainbow.prae@gmail.com>
2016-01-18 17:19:43 +0700

    Create View
    -index
    -reportDetail
    -stageDetail
    but just like <li>
    
    **has problem in checking completed Process through Report

--- process/urls.py
+++ process/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'process'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),    
+    url(r'^(?P<pk>[0-9]+)/$', views.reportDetail.as_view(), name='reportDetail'),    
+    url(r'^(?P<pk>[0-9]+)/stage/$', views.stageDetail.as_view(), name='stageDetail'),
 ]
--- process/views.py
+++ process/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.utils import timezone
+from .models import Report, Stage
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+class IndexView(generic.ListView):
+    template_name = 'process/index.html'
+    context_object_name = 'latest_report_list'
+
+    def get_queryset(self):
+	    """
+	    Return the last five published questions (not including those set to be
+	    published in the future).
+	    """
+	    return Report.objects.order_by('-endDate')[:5]
+
+class reportDetail(generic.DetailView):
+    model = Report
+    template_name = 'process/reportDetail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Report.objects.all()
+
+class stageDetail(generic.DetailView):
+    model = Stage
+    template_name = 'process/stageDetail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Stage.objects.all()
